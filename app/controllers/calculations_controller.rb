bbc9be549e5e047107bd186b9e0e7902320e2030
Grant Ziesemer <gzieseme@chicagobooth.edu>
2017-07-13 01:57:25 +0000

    form input works

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,4 +13,22 @@ class CalculationsController < ApplicationController
         @squared_number = (@user_number)**(0.5)
         render("calculations/flex_square_root_template.html.erb")
     end
+    def flex_payment
+        #incoming actions look like this [:"a_number"=>"5"]
+        #rails stores that has in a variable called params
+        @interest=params["a_number"].to_i
+        @term=params["b_number"].to_i
+        @principle=params["c_number"].to_i
+        
+        render("calculations/flex_payment_template.html.erb")
+    end
+    def square_form
+
+        render("calculations/square_form_template.html.erb")
+    end
+    def square_form_results
+        @user_number=params["the_user_number"].to_i
+
+        render("calculations/square_form_results_template.html.erb")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get("/flex/square/:a_number", { :controller => "calculations", :action => "flex_square" })
-  get("/flex/squareroot/:a_number", { :controller => "calculations", :action => "flex_square_root" })
+  get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square" })
+  get("/flexible/squareroot/:a_number", { :controller => "calculations", :action => "flex_square_root" })
+  get("/flexible/payment/:a_number/:b_number/:c_number", { :controller => "calculations", :action => "flex_payment" })
+  get("/square/new", { :controller => "calculations", :action => "square_form" })
+  get("/square/results", { :controller => "calculations", :action => "square_form_results" })
 
 
   mount WebGit::Engine, at: "/rails/git"
